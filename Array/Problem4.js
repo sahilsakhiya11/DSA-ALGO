72bdd8b471b3a3d8315c6857d82786eaeb2be7bf
Sahil <spatel@tvo.org>
2022-03-02 22:10:40 -0500

    Sort the array

---
+++ Array/Problem4.js
@@ -0,0 +1,8 @@
+// Problem:- Given an array which consists only 0,1,2. Sort the array
+
+const sortArray = (arr) => {
+    console.log(arr.sort())
+}
+
+const array = [0,2,1,2,1,2,1,2,0,0,1,2]
+sortArray(array)
